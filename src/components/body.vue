b42633eb43a67c49b4f0cde2f4dd72c42528981f
Promise Okeke <ugobright31@gmail.com>
2024-12-23 10:39:19 +0100

    removed unnecessary battery as well as structured files appropriately

---
+++ src/components/body.vue
@@ -0,0 +1,16 @@
+
+<script>
+
+</script>
+
+<template>
+    <div>
+        <p>hello world</p>
+    </div>
+</template>
+<style scoped>
+/* Add your styles here */
+p{
+    color: green;
+}
+</style>
---
+++ src/components/sidebarNav.vue
@@ -0,0 +1,28 @@
+<script>
+    import writingIcon from '../assets/icons/writingIcon.png'
+</script>
+
+<template>
+   <div class="sidebar_nav">   
+        <div class="sidebar_links d-flex flex-column">
+            <button class="link d-flex align-items-center flex-row">
+                <img src="../assets/icons/writingIcon.png" alt="">
+                Log expenses</button>
+            <button class="link d-flex align-items-center flex-row">
+                <img src="../assets/icons/dashboardIcon.png" alt="">
+                Dashboard</button>
+            <button class="link d-flex align-items-center flex-row">
+                <img src="../assets/icons/profileIcon.png" alt="">
+                Profile</button>
+            <button class="link d-flex align-items-center flex-row">
+                <img src="../assets/icons/logoutIcon.png" alt="">
+                Logout</button>
+        </div>
+
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
